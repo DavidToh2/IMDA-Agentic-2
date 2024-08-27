054e3730645b2052dd93f18259bbf2c22c3eb6fe
Shao Yang <shaoyang2002@gmail.com>
2024-08-27 11:38:52 +0800

    o

--- src/main.py
+++ src/main.py
@@ -1,17 +1,13 @@
-from LanggraphSingleAgent import ProfileGeneratorAgent
-from LanggraphAgentic import LanggraphAgent
+
+from AutogenAgentic import ProfileGeneratorAgent
 
 from chroma.ChromaDatabase import internal_search
 
 def main():
 
-    prompt = """Write a report of the Crowdstrike incident."""
-    detailed_instructions = """
-        Step 1. Conduct an online search of externally available data.
-        Step 2. Conduct an internal search of data in our internal database.
-        Step 3. Combine the results of the above two steps into a report. Separate the information gathered externally from the information gathered internally.
-    """
-    pf = LanggraphAgent(prompt, detailed_instructions)
+    prompt = """Generate a speaker profile for Dario Amodei."""
+    detailed_instructions = ""
+    pf = ProfileGeneratorAgent("Dario Amodei")
     pf.start()
 
     # print(internal_search("Dario Amodei"))
